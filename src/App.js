fb0b694f5d33ef7676a2cf11fbfe472c3671cd91
nduitsman <nduitsman@gmail.com>
2022-09-16 20:30:49 -0400

    css styling to home page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import './App.css';
-import Home from './pages/Home'
+import './App.css'
+import Home from './pages/Home';
 import MoviesList from './components/MoviesList';
-import Trending from './components/Trending';
-import TopRated from './components/TopRated';
-import Popular from './components/Popular';
-import Upcoming from './components/Upcoming';
-import NowPlaying from './components/NowPlaying';
+
 
 
 function App() {
   return (
     <div className="App">
-      <MoviesList url = {process.env.REACT_APP_NOW_PLAYING_URL}/>
-      <MoviesList url = {process.env.REACT_APP_POPULAR_URL}/>
-      <MoviesList url = {process.env.REACT_APP_TOP_RATED_URL}/>
-      <MoviesList url = {process.env.REACT_APP_TRENDING_URL}/>
-      <MoviesList url = {process.env.REACT_APP_UPCOMING_URL}/>
+      <h1 className = 'horizontalList'>Trening Today</h1>
+      <MoviesList url = {process.env.REACT_APP_TRENDING_URL} heading={'Trending Today'}/>
+      <h1 className = 'horizontalList'>Popular Titles</h1>
+      <MoviesList url = {process.env.REACT_APP_POPULAR_URL} heading={'Popular Titles'}/>
+      <h1 className = 'horizontalList'>Top Rated</h1>
+      <MoviesList url = {process.env.REACT_APP_TOP_RATED_URL} heading={'Top Rated'}/>
+      <h1 className = 'horizontalList'>Upcoming</h1>
+      <MoviesList url = {process.env.REACT_APP_UPCOMING_URL} heading={'Upcoming'}/>
+      <h1 className = 'horizontalList'>Now Playing</h1>
+      <MoviesList url = {process.env.REACT_APP_NOW_PLAYING_URL} heading={'Now Playing'}/>
     </div>
   );
 }
