8527e3cc53bdf8fadac5856d855de143052be1ac
nduitsman <nduitsman@gmail.com>
2022-09-18 15:37:59 -0400

    fixed nav header css issue

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -2,7 +2,7 @@ import './Header.css';
 
 function Header() {
     return (
-        <div className="header">
+        <div className="navHeader">
             <div className="nav"><a href="#">Home</a><a href="#">Movies</a><a href="#">TV Series</a><a href="#">Kids</a><a href="#">Watch List</a></div>
             <div className="account"><img src="/icons/icons8-male-user.svg" alt="account-icon" /></div>
         </div>
