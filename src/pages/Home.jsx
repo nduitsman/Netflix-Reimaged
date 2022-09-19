3020b4ddd430bb6dce6aa2bf576bfce8d15cb4c6
nduitsman <nduitsman@gmail.com>
2022-09-19 11:25:29 -0400

    started show page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Header from '../components/home/Header';
 import Backdrop from '../components/home/Backdrop';
 import Main from '../components/home/Main';
 import MoviePopUp from '../components/home/MoviePopUp';
+import Show from '../components/home/Show';
 
 function Home() {
     return (
@@ -10,6 +11,7 @@ function Home() {
             <Header />
             
             <Main />
+            <Show apikey={ process.env.REACT_APP_API_KEY_OMDB} title={'fall'}/>
             <MoviePopUp apikey={ process.env.REACT_APP_API_KEY_TMDB}/>
         </div>
     )
