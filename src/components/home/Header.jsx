39e07507ac61e6501c9a97543563b812db9e2443
brendonvan <brendon@van.digital>
2022-09-20 13:55:44 -0700

    Watchlist routing for logined users

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,19 +1,22 @@
 import './Header.css';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { signIn, signOut } from '../../actions';
+import { useNavigate, Link } from 'react-router-dom'
 
 function Header() {
     const dispatch = useDispatch();
 
-    const activeUser = useSelector(state => state.userReducer)
+    const activeUser = useSelector(state => state.userReducer);
     
     console.log(activeUser);
 
     return (
         <div className="navHeader">
-            <div className="nav"><a href="#">Home</a><a href="#">All Movies</a><a href="#">Watch List</a></div>
+            <div className="nav"><a href="#">Home</a><a href="#">All Movies</a><Link to={ activeUser.username !== '' ? '/watchlist' : '/login' }>Watch List</Link></div>
             <p>{ activeUser.username }</p>
-            <div className="account"><img src="/icons/icons8-male-user.svg" alt="account-icon" /></div>
+            <Link to='/login'><div className="account"><img src="/icons/icons8-male-user.svg" alt="account-icon" /></div></Link>
+            
         </div>
     )
 }
