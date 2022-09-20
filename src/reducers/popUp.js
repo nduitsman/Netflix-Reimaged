ffd218d71a437487c5bcc586e14de352981ef788
brendonvan <brendon@van.digital>
2022-09-20 11:10:22 -0700

    Complete Background Video in popup and showpage

--- src/reducers/popUp.js
+++ src/reducers/popUp.js
@@ -1,5 +1,6 @@
 const initialState = {
     movieId: 532639,
+    trailerId: 'imv03rS6Vb0',
     title: 'Pinocchio',
     isHidden: true
 }
@@ -7,9 +8,12 @@ const initialState = {
 const popUpReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'SHOW':
-            return { ...state, movieId: action.payload.id, title: action.payload.title, isHidden: false };
+            return { ...state, movieId: action.payload.id, title: action.payload.title, isHidden: false, trailerId: action.payload.trailerId };
         case 'HIDE':
             return { ...state, isHidden: true };
+        case 'UPDATE_ID':
+            // console.log(action.payload)
+            return {...state, trailerId: action.payload}
         default: 
             return state;
     }
