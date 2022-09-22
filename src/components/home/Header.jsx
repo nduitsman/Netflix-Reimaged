8720894e90256e9c2f1e89eabee8b0fe929f3ea5
nduitsman <nduitsman@gmail.com>
2022-09-22 11:48:09 -0400

    styling updates

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -8,20 +8,35 @@ function Header() {
     const dispatch = useDispatch();
 
     const activeUser = useSelector(state => state.userReducer);
-    
+
     return (
         <div className="navHeader">
-            
-            <div className="nav"><img src="/icons/icons8-netflix (1).svg" alt="home-logo" /><Link to={ '/' }>Home</Link><Link to={ '/movies' }>All Movies</Link><Link to={ activeUser.username !== '' ? '/watchlist' : '/login' }>Watch List</Link></div>
-            
+
+            <div className="nav">
+                <div className = 'logos'>
+                    <img src="/icons/icons8-netflix (1).svg" alt="home-logo" className='logoT'/>
+                    <img src='https://fontmeme.com/permalink/220922/08ec9f704160b7fa96e6b73e4f459b6b.png' alt='home-logo' className='logoB'/>
+                </div>
+
+
+                <Link to={'/'}>Home</Link>
+                <Link to={'/movies'}>All Movies</Link>
+                <Link to={activeUser.username !== '' ? '/watchlist' : '/login'}>Watch List</Link>
+            </div>
+
             <div className="account">
                 {console.log(activeUser.username)}
-                {(activeUser.username) ? <p>{ activeUser.username }</p> : <Link to='/login'><p>Login/Register</p></Link>}
-                
-                <Link to='/login'><img src="/icons/icons8-male-user.svg" alt="account-icon" /></Link>
+                {(activeUser.username) ? <p>{activeUser.username}</p> : <Link to='/login'><p>Login/Register</p></Link>}
+                <div className='dropdown'>
+                    <img src="/icons/icons8-male-user.svg" alt="account-icon" />
+                    <div className='dropdown-elements'>
+                        <Link to='/watchlist' className='dropdown-element'><p>Watchlist</p></Link>
+                        <Link to='/logout' className='dropdown-element'><p>Logout</p></Link>
+                    </div>
+                </div>
             </div>
         </div>
-        
+
     )
 }
 
