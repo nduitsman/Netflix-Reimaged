230c646c97f60abaa7f831aeb78d716ced28afc4
SeanBu <seanbuchas@gmail.com>
2022-09-22 12:28:37 -0400

    setup props

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,31 +9,31 @@ import MoviePopUp from '../components/home/MoviePopUp';
 import Show from '../components/home/Show';
 import Footer from '../components/home/Footer';
 
-function Home() {
-    const dispatch = useDispatch();
-  
+function Home(props) {
+  const dispatch = useDispatch();
 
 
-    useEffect(() => {
-  
-        const handleMovement = event => {
-          dispatch(hidePopUp());
-        };
-    
-        window.addEventListener('scroll', handleMovement);
-      }, []);
-    
 
-    return (
-        <div className='home-page'>
-            <Backdrop />
-            <Header />
-            <Main />
-            <Show apikey={ process.env.REACT_APP_API_KEY_OMDB} />
-            <MoviePopUp apikey={ process.env.REACT_APP_API_KEY_TMDB}/>
-            <Footer /> 
-        </div>
-    )
+  useEffect(() => {
+
+    const handleMovement = event => {
+      dispatch(hidePopUp());
+    };
+
+    window.addEventListener('scroll', handleMovement);
+  }, []);
+
+
+  return (
+    <div className='home-page'>
+      <Backdrop />
+      <Header logout={props.logout} deleteAccount={props.deleteAccount} />
+      <Main />
+      <Show apikey={process.env.REACT_APP_API_KEY_OMDB} />
+      <MoviePopUp apikey={process.env.REACT_APP_API_KEY_TMDB} />
+      <Footer />
+    </div>
+  )
 }
 
 export default Home;
